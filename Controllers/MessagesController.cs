c12d0059055794fbee760811765b931b52623aab
Myles Reid <myles@reidsmailbox.com>
2025-02-19 11:29:48 -0600

    Added DirectMessage Index Updated HEader

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InstagramClone.Controllers
+{
+    public class MessagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
